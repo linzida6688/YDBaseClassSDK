6c9ac0d2f99ee31f4415c341115d4c25c483eb18
lindashuai <1196917967@qq.com>
2020-03-09 11:26:55 +0800

    第一次提交

---
+++ YDBaseClassSDK/Classes/LzlTestTwo.h
@@ -0,0 +1,18 @@
+//
+//  LzlTestTwo.h
+//  FBSnapshotTestCase
+//
+//  Created by Lindashuai on 2020/3/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LzlTestTwo : NSObject
+
++ (void)showTwo;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YDBaseClassSDK/Classes/LzlTestTwo.m
@@ -0,0 +1,18 @@
+//
+//  LzlTestTwo.m
+//  FBSnapshotTestCase
+//
+//  Created by Lindashuai on 2020/3/9.
+//
+
+#import "LzlTestTwo.h"
+
+@implementation LzlTestTwo
+
++ (void)showTwo {
+    
+    NSLog(@"showTwo");
+    
+}
+
+@end
